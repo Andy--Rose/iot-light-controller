03ddd9ed9db1cabf72491cc12a961d5e4ea9a3cb
Andy Rose <arose@medallia.com>
2016-10-27 14:12:36 -0600

    Setup and Header

---
+++ app/components/header.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+	render() {
+		var headerStyle = {
+			color: this.props.secondaryColor,
+			background: this.props.primaryColor
+		};
+		return (
+			<div className="header" style={headerStyle}>
+				<div className="left">{this.props.name}</div>
+				<div className="right">v-{this.props.version}</div>
+			</div>
+		)
+	}
+}
+
+export default Header;
---
+++ app/containers/app.container.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Header from '../components/header.component';
+
+class AppContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			appName: 'IoT Light Controller',
+			appVersion: '1.0.0',
+			primaryColor: 'rgb(0, 0, 200)',
+			secondaryColor: 'rgb(255, 255, 255)',
+			wheelPosition: 224,
+			brightness: 100,
+			currentDevice: 'Living Room',
+			devices: [
+				'Living Room'
+			],
+			currentPreset: '',
+			presets: [
+				'Ambient'
+			]
+		}
+	}
+
+	sendColor(device, color) {
+
+	}
+
+	sendCommand(device, command) {
+
+	}
+
+	render() {
+		return (
+			<div className="app-container">
+				<Header
+					name={this.state.appName}
+					version={this.state.appVersion}
+					primaryColor={this.state.primaryColor}
+					secondaryColor={this.state.secondaryColor}
+				/>
+			</div>
+		)
+	}
+}
+
+export default AppContainer
